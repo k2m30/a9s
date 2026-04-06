7de20c910be66b385a92c9f70144d19ae2b73b59
Mikhail Chuprynski <1m@tut.by>
2026-04-06 13:46:08 +0200

    feat: related-view for Kinesis Data Streams (kinesis) (#154)

---
+++ internal/aws/kinesis_related.go
@@ -0,0 +1,63 @@
+// kinesis_related.go contains Kinesis Data Stream related-resource checker functions.
+package aws
+
+import (
+	"context"
+
+	cwtypes "github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
+
+	"github.com/k2m30/a9s/v3/internal/resource"
+)
+
+func init() {
+	resource.RegisterRelated("kinesis", []resource.RelatedDef{
+		{TargetType: "lambda", DisplayName: "Lambda Functions", Checker: nil, NeedsTargetCache: false},
+		{TargetType: "alarm", DisplayName: "CW Alarms", Checker: checkKinesisAlarms, NeedsTargetCache: true},
+		{TargetType: "cfn", DisplayName: "CloudFormation", Checker: nil, NeedsTargetCache: false},
+	})
+}
+
+// checkKinesisAlarms checks the cache for CloudWatch alarms with StreamName dimension matching this stream.
+func checkKinesisAlarms(ctx context.Context, clients any, res resource.Resource, cache resource.ResourceCache) resource.RelatedCheckResult {
+	streamName := res.ID
+	if streamName == "" {
+		return resource.RelatedCheckResult{TargetType: "alarm", Count: 0}
+	}
+
+	alarmList, truncated, err := kinesisRelatedResources(ctx, clients, cache, "alarm")
+	if err != nil {
+		return resource.RelatedCheckResult{TargetType: "alarm", Count: -1, Err: err}
+	}
+	if alarmList == nil {
+		return resource.RelatedCheckResult{TargetType: "alarm", Count: -1}
+	}
+
+	var ids []string
+	for _, alarmRes := range alarmList {
+		rawAlarm, ok := assertStruct[cwtypes.MetricAlarm](alarmRes.RawStruct)
+		if !ok {
+			continue
+		}
+		for _, d := range rawAlarm.Dimensions {
+			if d.Name != nil && *d.Name == "StreamName" && d.Value != nil && *d.Value == streamName {
+				ids = append(ids, alarmRes.ID)
+				break
+			}
+		}
+	}
+	if len(ids) == 0 && truncated {
+		return resource.RelatedCheckResult{TargetType: "alarm", Count: -1}
+	}
+	return relatedResult("alarm", ids)
+}
+
+// kinesisRelatedResources returns the resource list for target from cache or by fetching the first page.
+func kinesisRelatedResources(ctx context.Context, clients any, cache resource.ResourceCache, target string) ([]resource.Resource, bool, error) {
+	resources, isTruncated, err := FetchRelatedTarget(ctx, clients, cache, target)
+	if err != nil {
+		if _, ok := clients.(*ServiceClients); !ok {
+			return nil, false, nil
+		}
+	}
+	return resources, isTruncated, err
+}
